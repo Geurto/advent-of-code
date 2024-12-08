a7b3a522bf9e235a2a3ada6028eb20517be42e3a
peter-ranmarine <peter@ranmarine.io>
2024-12-08 21:09:28 +0100

    d4p2 done

--- 2024/day_4/src/main.rs
+++ 2024/day_4/src/main.rs
@@ -6,6 +6,7 @@ struct LetterGrid {
     width: usize,
     height: usize,
     xmas_hits: u32,
+    cross_mas_hits: u32,
 }
 
 impl LetterGrid {
@@ -19,6 +20,8 @@ impl LetterGrid {
         (1, 0),
         (1, 1),
     ];
+
+    const CROSS_DIRECTIONS: [(i32, i32); 4] = [(-1, -1), (-1, 1), (1, -1), (1, 1)]; // A from M
 
     fn new(lines: Vec<String>) -> Self {
         let height = lines.len();
@@ -42,6 +45,7 @@ impl LetterGrid {
             width,
             height,
             xmas_hits: 0,
+            cross_mas_hits: 0,
         }
     }
 
@@ -49,12 +53,8 @@ impl LetterGrid {
         row >= 0 && row < self.height as i32 && col >= 0 && col < self.width as i32
     }
 
-    fn get_letter(&self, row: i32, col: i32) -> Option<&char> {
-        if self.is_in_bounds(row, col) {
-            Some(&self.grid[row as usize][col as usize])
-        } else {
-            None
-        }
+    fn check_letter(&self, row: i32, col: i32, desired: char) -> bool {
+        self.is_in_bounds(row, col) && self.grid[row as usize][col as usize] == desired
     }
 
     fn find_xmas_patterns(&mut self) {
@@ -71,44 +71,77 @@ impl LetterGrid {
         let base_row = row as i32;
         let base_col = col as i32;
 
-        for &(dx, dy) in Self::DIRECTIONS.iter() {
-            let m_row = base_row + dx;
-            let m_col = base_col + dy;
+        for &(dy, dx) in Self::DIRECTIONS.iter() {
+            let m_row = base_row + dy;
+            let m_col = base_col + dx;
 
-            let m_letter = if let Some(x) = self.get_letter(m_row, m_col) {
-                x
-            } else {
-                continue;
-            };
-            if *m_letter != 'M' {
+            if !self.check_letter(m_row, m_col, 'M') {
                 continue;
             }
 
-            let a_row = m_row + dx;
-            let a_col = m_col + dy;
+            let a_row = m_row + dy;
+            let a_col = m_col + dx;
 
-            let a_letter = if let Some(x) = self.get_letter(a_row, a_col) {
-                x
-            } else {
-                continue;
-            };
-            if *a_letter != 'A' {
+            if !self.check_letter(a_row, a_col, 'A') {
                 continue;
             }
 
-            let s_row = a_row + dx;
-            let s_col = a_col + dy;
+            let s_row = a_row + dy;
+            let s_col = a_col + dx;
 
-            let s_letter = if let Some(x) = self.get_letter(s_row, s_col) {
-                x
-            } else {
-                continue;
-            };
-            if *s_letter != 'S' {
+            if !self.check_letter(s_row, s_col, 'S') {
                 continue;
             }
 
             self.xmas_hits += 1;
+        }
+    }
+
+    fn find_cross_mas_patterns(&mut self) {
+        for row in 0..self.height {
+            for col in 0..self.width {
+                if self.grid[row][col] == 'M' {
+                    self.check_cross_mas_from_position(row, col);
+                }
+            }
+        }
+    }
+
+    fn check_cross_mas_from_position(&mut self, row: usize, col: usize) {
+        let base_row = row as i32;
+        let base_col = col as i32;
+
+        for &(dy, dx) in Self::CROSS_DIRECTIONS.iter() {
+            // check for A
+            let a_row = base_row + dy;
+            let a_col = base_col + dx;
+
+            if !self.check_letter(a_row, a_col, 'A') {
+                continue;
+            }
+
+            let ms_row = base_row + 2 * dy;
+            let ms_col = base_col + 2 * dx;
+
+            // need an S across
+            if !self.check_letter(ms_row, ms_col, 'S') {
+                continue;
+            }
+
+            // check letter on same row
+            if self.check_letter(base_row, ms_col, 'M') {
+                // need an S on the same column
+                if !self.check_letter(ms_row, base_col, 'S') {
+                    continue;
+                }
+                self.cross_mas_hits += 1;
+            } else if self.check_letter(base_row, ms_col, 'S') {
+                // need an M on the same column
+                if !self.check_letter(ms_row, base_col, 'M') {
+                    continue;
+                }
+                self.cross_mas_hits += 1;
+            }
         }
     }
 }
@@ -120,9 +153,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
     let mut letter_grid = LetterGrid::new(lines);
 
+    // Part 1
     letter_grid.find_xmas_patterns();
-
     println!("Found {} XMAS patterns", letter_grid.xmas_hits);
+
+    // Part 2: checking every M means we find 2N patterns
+    letter_grid.find_cross_mas_patterns();
+    println!(
+        "Found {} crossed MAS patterns",
+        letter_grid.cross_mas_hits / 2
+    );
 
     Ok(())
 }
