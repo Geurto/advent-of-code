a6008c3dfd0c0aa9de4b31b89f3a80373b6a6eba
peter-ranmarine <peter@ranmarine.io>
2024-12-12 12:16:10 +0100

    smol start d7p1

--- 2024/day_7/src/main.rs
+++ 2024/day_7/src/main.rs
@@ -1,41 +1,52 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-struct Equation {
-    result: u64,
-    factors: Vec<u16>,
-}
-
-impl Equation {
-    fn new(line: &str) -> Self {
-        let split: Vec<&str> = line.split(": ").collect();
-        let result = split[0].parse::<u64>().unwrap();
-        let mut factors: Vec<u16> = split[1]
-            .split(" ")
-            .map(|s| s.parse::<u16>().unwrap())
-            .collect();
-
-        println!("Factors: {:?}, result: {}", factors, result);
-        Equation { result, factors }
-    }
-
-    fn solve(&mut self) {
-        for &f in self.factors.iter().rev() {
-            if self.result % f as u64 == 0 {
-                println!("{} is a factor of {}", f, self.result);
-            }
-        }
-    }
-}
-
 fn main() -> std::io::Result<()> {
-    let file = File::open("data/dummy")?;
+    let file = File::open("data/input")?;
     let reader = BufReader::new(file);
+    let mut sum_solvable_results = 0;
 
     for line in reader.lines().map_while(Result::ok) {
-        let mut eq = Equation::new(&line);
-        eq.solve();
+        let split: Vec<&str> = line.split(": ").collect();
+        let result = split[0].parse::<usize>().unwrap();
+        let factors: Vec<usize> = split[1]
+            .split(" ")
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
+
+        if subdivide((result, &factors)) {
+            sum_solvable_results += result;
+        }
     }
 
+    println!("Sum of solvable results: {}", sum_solvable_results);
+
     Ok(())
+}
+
+fn subdivide(equation: (usize, &[usize])) -> bool {
+    let (result, factors) = equation;
+
+    if factors.is_empty() {
+        return false;
+    } else if factors.len() == 1 {
+        return result == factors[0];
+    }
+
+    let k = factors[factors.len() - 1];
+
+    if result == k {
+        return true;
+    }
+
+    if result < k {
+        return false;
+    }
+
+    // if k is a factor, then continue to subdivide
+    if result % k == 0 {
+        return subdivide((result / k, &factors[..factors.len() - 1]));
+    }
+
+    subdivide((result - k, &factors[..factors.len() - 1]))
 }
