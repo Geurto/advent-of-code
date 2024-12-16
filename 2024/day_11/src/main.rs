74f2385373f2998825356757d858913c39d5bf92
peter-ranmarine <peter@ranmarine.io>
2024-12-16 22:02:00 +0100

    starting to understand day 11 part 2

--- 2024/day_11/src/main.rs
+++ 2024/day_11/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::fs;
 
 use env_logger::Builder;
-use log::info;
+use log::{debug, info};
 
 fn main() {
     Builder::new().filter_level(log::LevelFilter::Debug).init();
@@ -12,23 +12,48 @@ fn main() {
         .split_whitespace()
         .map(|x| x.parse().unwrap())
         .collect();
-    let mut stones_map: HashMap<usize, Vec<usize>> = HashMap::new();
-    stones_map.insert(0, vec![1]);
+
+    // index, (count, next)
+    let mut hashmap: HashMap<usize, (usize, Vec<usize>)> = HashMap::new();
+    hashmap.insert(0, (0, vec![1]));
+    hashmap.insert(1, (0, vec![2024]));
 
     for i in 0..25 {
-        info!("#{}: {} stones", i, stones.len());
-        blink(&mut stones, &mut stones_map);
+        if i < 10 {
+            debug!("Stones: {:?}", stones);
+        }
+        blink(&mut stones, &mut hashmap);
+        info!("# {}.    {} stones", i + 1, stones.len());
     }
+    debug!("Hashmap size: {}", hashmap.len());
+    debug!(
+        "First 10 elements of hashmap: {:?}",
+        hashmap.iter().collect::<Vec<_>>()
+    );
 }
 
-fn blink(stones: &mut Vec<usize>, map: &mut HashMap<usize, Vec<usize>>) {
-    let stones_taken = std::mem::take(stones);
-    for (i, s) in stones_taken.into_iter().enumerate() {
-        if let Some(new_stones) = map.get(&s) {
-            stones[i] = new_stones[0];
-            if new_stones.len() == 2 {
-                stones.push(new_stones[1]);
+fn blink(stones: &mut Vec<usize>, map: &mut HashMap<usize, (usize, Vec<usize>)>) {
+    let mut new_stones = Vec::new();
+
+    for s in stones.iter_mut() {
+        let (count, next_stones) = map.entry(*s).or_insert_with(|| {
+            let digits = (*s as f64).log10() as usize + 1;
+            if digits % 2 == 0 {
+                let divisor = 10usize.pow(digits as u32 / 2);
+                let (left, right) = (*s / divisor, *s % divisor);
+                (0, vec![left, right])
+            } else {
+                (0, vec![*s * 2024])
             }
+        });
+
+        *count += 1;
+        let next_stones = next_stones.clone();
+
+        *s = next_stones[0];
+        if next_stones.len() > 1 {
+            new_stones.push(next_stones[1]);
         }
     }
+    stones.append(&mut new_stones);
 }
