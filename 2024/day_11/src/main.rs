03d4f8b44eb57f7046bd5a92c9a3031f0661f4df
peter-ranmarine <peter@ranmarine.io>
2024-12-16 22:26:48 +0100

    day 11 done

--- 2024/day_11/src/main.rs
+++ 2024/day_11/src/main.rs
@@ -7,53 +7,65 @@ use log::{debug, info};
 fn main() {
     Builder::new().filter_level(log::LevelFilter::Debug).init();
 
-    let data = fs::read_to_string("data/dummy").expect("Unable to read file");
-    let mut stones: Vec<usize> = data
+    let data = fs::read_to_string("data/input").expect("Unable to read file");
+    let stones: Vec<usize> = data
         .split_whitespace()
         .map(|x| x.parse().unwrap())
         .collect();
 
     // index, (count, next)
-    let mut hashmap: HashMap<usize, (usize, Vec<usize>)> = HashMap::new();
-    hashmap.insert(0, (0, vec![1]));
-    hashmap.insert(1, (0, vec![2024]));
+    let mut result_map: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut stones_map: HashMap<usize, usize> = HashMap::new();
 
-    for i in 0..25 {
-        if i < 10 {
-            debug!("Stones: {:?}", stones);
-        }
-        blink(&mut stones, &mut hashmap);
-        info!("# {}.    {} stones", i + 1, stones.len());
+    // initialize stones
+    for s in stones.iter() {
+        let count = stones_map.entry(*s).or_insert(0);
+        *count += 1;
     }
-    debug!("Hashmap size: {}", hashmap.len());
+
+    // initialize trivial results
+    result_map.insert(0, vec![1]);
+    result_map.insert(1, vec![2024]);
+
+    for i in 0..75 {
+        stones_map = blink(&mut stones_map, &mut result_map);
+        let num_stones = stones_map.values().sum::<usize>();
+        info!("# {}.    {} stones", i + 1, num_stones);
+    }
+    debug!("Hashmap size: {}", result_map.len());
     debug!(
-        "First 10 elements of hashmap: {:?}",
-        hashmap.iter().collect::<Vec<_>>()
+        "Result hashmap: {:?}",
+        result_map.iter().collect::<Vec<_>>()
     );
 }
 
-fn blink(stones: &mut Vec<usize>, map: &mut HashMap<usize, (usize, Vec<usize>)>) {
-    let mut new_stones = Vec::new();
+fn blink(
+    previous_stones: &mut HashMap<usize, usize>,
+    map: &mut HashMap<usize, Vec<usize>>,
+) -> HashMap<usize, usize> {
+    let mut new_counts: HashMap<usize, usize> = HashMap::new();
 
-    for s in stones.iter_mut() {
-        let (count, next_stones) = map.entry(*s).or_insert_with(|| {
+    // check each unique number on previous stones
+    for (s, stone_count) in previous_stones.iter_mut() {
+        let next_stones = map.entry(*s).or_insert_with(|| {
             let digits = (*s as f64).log10() as usize + 1;
             if digits % 2 == 0 {
                 let divisor = 10usize.pow(digits as u32 / 2);
                 let (left, right) = (*s / divisor, *s % divisor);
-                (0, vec![left, right])
+                vec![left, right]
             } else {
-                (0, vec![*s * 2024])
+                vec![*s * 2024]
             }
         });
 
-        *count += 1;
-        let next_stones = next_stones.clone();
+        let count = new_counts.entry(next_stones[0]).or_insert(0);
+        *count += *stone_count;
 
-        *s = next_stones[0];
-        if next_stones.len() > 1 {
-            new_stones.push(next_stones[1]);
+        if next_stones.len() == 2 {
+            let count = new_counts.entry(next_stones[1]).or_insert(0);
+            *count += *stone_count;
         }
     }
-    stones.append(&mut new_stones);
+
+    new_counts
 }
